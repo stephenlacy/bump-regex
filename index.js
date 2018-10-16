b3cfd1572931c87a733109ba287696b51ebf558f
Jimmy Wärting <jimmy@warting.se>
2018-10-16 20:00:06 +0200

    removed xtend (#24)
    * removed xtend
    
    * updated readme

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var semver = require('semver');
-var extend = require('xtend')
 
 module.exports = function(options, cb) {
   if (typeof options === 'string') {
@@ -18,7 +17,7 @@ module.exports = function(options, cb) {
     keepmetadata: false
   }
 
-  var opts = extend(defaultOpts, options);
+  var opts = Object.assign({}, defaultOpts, options);
 
   var keyRegex = opts.key
 
@@ -32,16 +31,16 @@ module.exports = function(options, cb) {
       '([<|\'|\"]?(',
       keyRegex,
       ')[>|\'|\"]?[ ]*[:=]?[ |>]*[\'|\"]?[a-z]?)',
-      
+
       // Match Semver version identifier, e.g.: x.y.z
       '(\\d+\\.\\d+\\.\\d+)',
-      
+
       // Match Semver pre-release identifier, e.g. -pre.0-1
       '(-[0-9A-Za-z\.-]+)?',
-      
+
       // Match Semver metadata identifier, e.g. +meta.0-1
       '(\\+[0-9A-Za-z\.-]+)?',
-      
+
       // Match end of version value: e.g. ", ', <
       '([\'|\"|<]?)'
     ].join(''), + opts.case ? '' : 'i'
@@ -66,11 +65,11 @@ module.exports = function(options, cb) {
     if (opts.version) {
       opts.type = semver.diff(opts.prev, opts.new);
     }
-    
+
     if (!opts.keepmetadata || !!opts.version) {
       metadata = '';
     }
-    
+
     return prefix + version + (metadata || '') + (suffix || '');
   });
 
